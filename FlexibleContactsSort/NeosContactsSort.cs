701f946ef36f8794fdfbfc242c6d37cafed8eb94
Arne Kiesewetter <banane9@live.de>
2022-08-05 20:18:23 +0200

    Make sorting priorities configurable

---
+++ FlexibleContactsSort/NeosContactsSort.cs
@@ -0,0 +1,179 @@
+﻿using BaseX;
+using CloudX.Shared;
+using FrooxEngine;
+using HarmonyLib;
+using NeosModLoader;
+using System;
+
+namespace FlexibleContactsSort
+{
+    public class FlexibleContactsSort : NeosMod
+    {
+        public static ModConfiguration Config;
+
+        [AutoRegisterConfigKey]
+        private static ModConfigurationKey<int> AlphabeticPriority = new ModConfigurationKey<int>("AlphabeticPriority", "Priority of the contact's name. Set 0 to ignore; negative to invert.", () => 1);
+
+        [AutoRegisterConfigKey]
+        private static ModConfigurationKey<int> OnlineStatusPriority = new ModConfigurationKey<int>("OnlineStatusPriority", "Priority of the contact's online status. Set 0 to ignore; negative to invert.", () => 10);
+
+        [AutoRegisterConfigKey]
+        private static ModConfigurationKey<int> JoinablePriority = new ModConfigurationKey<int>("JoinablePriority", "Priority of the contact being in a session you can join. Set 0 to ignore; negative to invert.", () => 0);
+
+        [AutoRegisterConfigKey]
+        private static ModConfigurationKey<int> ContactRequestPriority = new ModConfigurationKey<int>("ContactRequestPriority", "Priority of the contact being a new request. Set 0 to ignore; negative to invert.", () => 1000);
+
+        [AutoRegisterConfigKey]
+        private static ModConfigurationKey<int> UnreadMsgPriority = new ModConfigurationKey<int>("UnreadMsgPriority", "Priority of the contact having an unread message. Set 0 to ignore; negative to invert.", () => 10000);
+
+        public override string Name => "FlexibleContactsSort";
+        public override string Author => "Banane9";
+        public override string Version => "2.0.0";
+        public override string Link => "https://github.com/Banane9/NeosFlexibleContactsSort";
+
+        public override void OnEngineInit()
+        {
+#if DEBUG
+            Warn($"Extremely verbose debug logging is enabled in this build. This probably means Banane9 messed up and gave you a debug build.");
+#endif
+            Harmony harmony = new Harmony($"{Author}.{Name}");
+            Config = GetConfiguration();
+            Config.Save(true);
+            harmony.PatchAll();
+        }
+
+        [HarmonyPatch(typeof(FriendsDialog), "OnCommonUpdate")]
+        private static class FriendsDialogPatch
+        {
+            public static void Prefix(ref bool ___sortList, out bool __state)
+            {
+                // steal the sortList bool's value, and force it to false from Neos's perspective
+                __state = ___sortList;
+                ___sortList = false;
+            }
+
+            public static void Postfix(bool __state, SyncRef<Slot> ____listRoot)
+            {
+                // if Neos would have sorted (but we prevented it)
+                if (__state)
+                {
+                    // we need to sort
+                    ____listRoot.Target.SortChildren((slot1, slot2) =>
+                    {
+                        var friendItem1 = slot1.GetComponent<FriendItem>();
+                        var friendItem2 = slot2.GetComponent<FriendItem>();
+                        var friend1 = friendItem1?.Friend;
+                        var friend2 = friendItem2?.Friend;
+
+                        // nulls go last, no need to build score
+                        if (friend1 != null && friend2 == null) return -1;
+                        if (friend1 == null && friend2 != null) return 1;
+                        if (friend1 == null && friend2 == null) return 0;
+
+                        var score1 = CalculateFriendOrderScore(friendItem1!);
+                        var score2 = CalculateFriendOrderScore(friendItem2!);
+
+                        // sort by name
+                        var alphabetical = string.Compare(friend1!.FriendUsername, friend2!.FriendUsername, StringComparison.CurrentCultureIgnoreCase);
+                        score1 += alphabetical * Config.GetValue(AlphabeticPriority);
+                        score2 -= alphabetical * Config.GetValue(AlphabeticPriority);
+
+                        return score1 - score2;
+                    });
+
+#if DEBUG
+                    Debug("BIG FRIEND DEBUG:");
+                    foreach (Slot slot in ____listRoot.Target.Children)
+                    {
+                        FriendItem? component = slot.GetComponent<FriendItem>();
+                        Friend? friend = component?.Friend;
+                        if (friend != null)
+                        {
+                            Debug($"  {GetOnlineStatusOrderNumber(friend)}: \"{friend.FriendUsername}\" status={friend.FriendStatus} online={friend.UserStatus?.OnlineStatus} incoming={friend.IsAccepted}");
+                        }
+                    }
+#endif
+                }
+            }
+        }
+
+        private static int CalculateFriendOrderScore(FriendItem friendItem)
+        {
+            var friend = friendItem.Friend;
+
+            var score = friend.FriendUserId == "U-Neos" ? -100_000_000 : 0;
+            score += friend.FriendStatus == FriendStatus.SearchResult ? 100_000_000 : 0; // non-contact search results always at the end
+            score += IsOfflineContact(friend) ? 10_000_000 : 0; // offline friends before results
+            score += IsOutgoingRequest(friend) ? 9_000_000 : 0; // outgoing requests before offline
+
+            score += (IsIncomingRequest(friend) ? 0 : 1) * Config.GetValue(ContactRequestPriority);
+            score += (HasUnreadMessages(friendItem) ? 0 : 1) * Config.GetValue(UnreadMsgPriority);
+            score += (IsInJoinableSession(friend) ? 0 : 1) * Config.GetValue(JoinablePriority);
+            score += GetOnlineStatusOrderNumber(friend) * Config.GetValue(OnlineStatusPriority);
+
+            return score;
+        }
+
+        private static bool HasUnreadMessages(FriendItem friend)
+        {
+            return friend.HasMessages;
+        }
+
+        private static bool IsOutgoingRequest(Friend friend)
+        {
+            return friend.FriendStatus == FriendStatus.Accepted && !friend.IsAccepted;
+        }
+
+        private static bool IsIncomingRequest(Friend friend)
+        {
+            return friend.FriendStatus == FriendStatus.Requested;
+        }
+
+        private static bool IsOfflineContact(Friend friend)
+        {
+            var status = friend.UserStatus?.OnlineStatus ?? OnlineStatus.Offline;
+
+            return friend.FriendStatus == FriendStatus.Accepted && friend.IsAccepted
+                && status == OnlineStatus.Offline || status == OnlineStatus.Invisible;
+        }
+
+        [HarmonyPatch(typeof(NeosUIStyle), nameof(NeosUIStyle.GetStatusColor))]
+        private static class NeosUIStylePatch
+        {
+            private static void Postfix(Friend friend, ref color __result)
+            {
+                var onlineStatus = friend.UserStatus?.OnlineStatus ?? OnlineStatus.Offline;
+
+                if (onlineStatus == OnlineStatus.Offline && friend.FriendStatus == FriendStatus.Accepted && !friend.IsAccepted)
+                    __result = color.Yellow;
+            }
+        }
+
+        private static bool IsInJoinableSession(Friend friend)
+        {
+            return friend.UserStatus is UserStatus status
+                && status.CurrentSession != null && status?.CompatibilityHash == Engine.Current.CompatibilityHash;
+        }
+
+        // lower numbers appear earlier in the list
+        private static int GetOnlineStatusOrderNumber(Friend friend)
+        {
+            var status = friend.UserStatus?.OnlineStatus ?? OnlineStatus.Offline;
+            switch (status)
+            {
+                case OnlineStatus.Online:
+                    return 0;
+
+                case OnlineStatus.Away:
+                    return 1;
+
+                case OnlineStatus.Busy:
+                    return 2;
+
+                default:
+                    return 3;
+                    // unsure how people with no relation, ignored, or blocked will appear... but they'll end up here too
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,6 +1,7 @@
-# NeosContactsSort
+Flexible Contacts Sort
+======================
 
-A [NeosModLoader](https://github.com/zkxs/NeosModLoader) mod for [Neos VR](https://neos.com/) that sorts contacts Better™.
+A [NeosModLoader](https://github.com/zkxs/NeosModLoader) mod for [Neos VR](https://neos.com/) that sorts contacts Better™ and to your liking.
 
 Relevant Neos issue: [#2596](https://github.com/Neos-Metaverse/NeosPublic/issues/2596).
 
@@ -8,7 +9,7 @@ Relevant Neos issue: [#2596](https://github.com/Neos-Metaverse/NeosPublic/issues
 I've made a few noteworthy changes to the sorting order:
 - No longer sorts by most recent message timestamp
 - Incoming friend requests are now the first category, preceding Online friends
-- Neos Bot is now forced to the top of the Online friends category
+- Neos Bot is now forced to the top of the list
 - Sent Requests are separated from Offline friends, and have a yellow background color
 
 ### Vanilla Sort
@@ -22,20 +23,24 @@ I've made a few noteworthy changes to the sorting order:
 3. Further ties broken by most recent message
 4. Even further ties broken by username alphabetical order
 
-### Modded Sort
-1. Friends with unread messages
-2. Ties broken by online status
-   1. Incoming Friend Requests
-   2. Neos Bot
-   3. Online Friends
-   4. Away Friends
-   5. Busy Friends
-   6. Sent Requests (background color changed from gray to yellow!)
-   7. Offline Friends
-   8. Search results
-3. Further ties broken by username alphabetical order
+### Default Modded Sort
+Sort Order can be changed to liking. Ordering of friends can additionally include sorting by whether
+a contact is in a world you can just join.
+
+0. Neos Bot
+1. Unread messages
+2. Incoming Friend Requests
+3. Ties broken by online status
+   1. Online Friends
+   2. Away Friends
+   3. Busy Friends
+   
+4. Sent Requests (background color changed from gray to yellow!)
+5. Offline Friends
+6. Search results
+7. Further ties broken by username alphabetical order
 
 ## Installation
 1. Install [NeosModLoader](https://github.com/zkxs/NeosModLoader).
-2. Place [NeosContactsSort.dll](https://github.com/zkxs/NeosContactsSort/releases/latest/download/NeosContactsSort.dll) into your `nml_mods` folder. This folder should be at `C:\Program Files (x86)\Steam\steamapps\common\NeosVR\nml_mods` for a default install. You can create it if it's missing, or if you launch the game once with NeosModLoader installed it will create the folder for you.
+2. Place [FlexibleContactsSort.dll](https://github.com/Banane9/NeosFlexibleContactsSort/releases/latest/download/FlexibleContactsSort.dll) into your `nml_mods` folder. This folder should be at `C:\Program Files (x86)\Steam\steamapps\common\NeosVR\nml_mods` for a default install. You can create it if it's missing, or if you launch the game once with NeosModLoader installed it will create the folder for you.
 3. Start the game. If you want to verify that the mod is working you can check your Neos logs.
